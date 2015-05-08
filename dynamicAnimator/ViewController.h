4b09c74ebc0c24a40bf5386c3738ef9c51f4c5ea
bjarte <b.sjursen@gmail.com>
2015-05-08 23:34:30 +0200

    Initial Commit

---
+++ dynamicAnimator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dynamicAnimator
+//
+//  Created by Bjarte Sjursen on 08.05.15.
+//  Copyright (c) 2015 Bjarte Sjursen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
